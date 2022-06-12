285fe885d8ba3e5fd0f9aa80ab86e55b7f286519
fausio luis <fausioluis@live.com>
2022-06-12 18:36:01 +0200

    clean services

--- src/app/models/Service/AppService.ts
+++ src/app/models/Service/AppService.ts
@@ -1,11 +1,15 @@
 
-
-
+import { HttpClient, HttpHeaders} from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { Product } from "../Product";
+import { InputModel,Token } from "../InputModel";
+@Injectable({providedIn:'root'})
 
 export class services{
 
 
-    
+
 }
 
 
